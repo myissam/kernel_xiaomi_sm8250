7b53fef76d28b6519266623ee680d3d7b32f9e91
Park Ju Hyung <qkrwngud825@gmail.com>
2019-03-12 14:38:55 +0900

    msm_geni_serial: make ioctl always exclusive
    Potential concurrent accesses to vote_clock_on/off() functions
    may leave runtime PM in an unwanted state.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Diab Neiroukh <lazerl0rd@thezest.dev>
    Signed-off-by: dreamisbaka <jolinux.g@gmail.com>

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -240,6 +240,7 @@ struct msm_geni_serial_port {
 	enum uart_error_code uart_error;
 	struct work_struct work;
 	struct workqueue_struct *qwork;
+	struct mutex ioctl_mutex;
 };
 
 static void msm_geni_serial_worker(struct work_struct *work);
@@ -609,9 +610,11 @@ static void msm_geni_serial_worker(struct work_struct *work)
 static int msm_geni_serial_ioctl(struct uart_port *uport, unsigned int cmd,
 						unsigned long arg)
 {
-	int ret = -ENOIOCTLCMD;
 	struct msm_geni_serial_port *port = GET_DEV_PORT(uport);
 	enum uart_error_code uart_error;
+	int ret;
+
+	mutex_lock(&port->ioctl_mutex);
 
 	switch (cmd) {
 	case TIOCPMGET: {
@@ -638,8 +641,12 @@ static int msm_geni_serial_ioctl(struct uart_port *uport, unsigned int cmd,
 		break;
 	}
 	default:
+		ret = -ENOIOCTLCMD;
 		break;
 	}
+
+	mutex_unlock(&port->ioctl_mutex);
+
 	return ret;
 }
 
@@ -3865,6 +3872,7 @@ static int __init msm_geni_serial_init(void)
 		msm_geni_serial_ports[i].uport.ops = &msm_geni_serial_pops;
 		msm_geni_serial_ports[i].uport.flags = UPF_BOOT_AUTOCONF;
 		msm_geni_serial_ports[i].uport.line = i;
+		mutex_init(&msm_geni_serial_ports[i].ioctl_mutex);
 	}
 
 	for (i = 0; i < GENI_UART_CONS_PORTS; i++) {
@@ -3872,6 +3880,7 @@ static int __init msm_geni_serial_init(void)
 		msm_geni_console_port.uport.ops = &msm_geni_console_pops;
 		msm_geni_console_port.uport.flags = UPF_BOOT_AUTOCONF;
 		msm_geni_console_port.uport.line = i;
+		mutex_init(&msm_geni_console_port.ioctl_mutex);
 	}
 
 	ret = console_register(&msm_geni_console_driver);
