e75b8b3bf5fd2f6a94bf67504ca5fbeb952d15c1
Miguel de Dios <migueldedios@google.com>
2019-04-29 16:09:33 -0700

    kernel: sched: Mitigate non-boosted tasks preempting boosted tasks
    Currently when a boosted task is scheduled we use prefer_idle to try and
    get it to an idle core. Once it's scheduled, there is a possibility we
    can schedule a non-boosted task on the same core where the boosted task
    is running on. This change aims to mitigate that possibility by checking
    if the core we're targeting has a boosted task and if so, use the next
    best idle core instead.
    
    Bug: 131626264
    Change-Id: I3d321e1c71f96526f55f7f3a56e32db411311aa2
    Signed-off-by: Miguel de Dios <migueldedios@google.com>
    Signed-off-by: Julian Liu <wlootlxt123@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7158,6 +7158,7 @@ static void find_best_target(struct sched_domain *sd, cpumask_t *cpus,
 #if IS_ENABLED(CONFIG_MIHW)
 	struct root_domain *rd;
 #endif
+	struct task_struct *curr_tsk;
 
 	/*
 	 * In most cases, target_capacity tracks capacity_orig of the most
@@ -7570,6 +7571,13 @@ static void find_best_target(struct sched_domain *sd, cpumask_t *cpus,
 	 *   a) ACTIVE CPU: target_cpu
 	 *   b) IDLE CPU: best_idle_cpu
 	 */
+	if (target_cpu != -1 && !idle_cpu(target_cpu) &&
+			best_idle_cpu != -1) {
+		curr_tsk = READ_ONCE(cpu_rq(target_cpu)->curr);
+		if (curr_tsk && schedtune_task_boost_rcu_locked(curr_tsk)) {
+			target_cpu = best_idle_cpu;
+		}
+	}
 
 	if (prefer_idle && (best_idle_cpu != -1)) {
 		target_cpu = best_idle_cpu;
