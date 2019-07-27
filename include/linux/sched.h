0d0dbd3afc0f573a7b60ec1a01c05aff270e78f7
Julian Liu <wlootlxt123@gmail.com>
2019-07-28 02:32:43 +0800

    sched/tune: Expose sched_boost
    Signed-off-by: Julian Liu <wlootlxt123@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2296,6 +2296,8 @@ extern inline void restore_inherit_top_app(struct task_struct *p);
 int do_stune_boost(char *st_name, int boost, int *slot);
 int do_stune_sched_boost(char *st_name, int *slot);
 int reset_stune_boost(char *st_name, int slot);
+
+int inline get_sched_boost(char *st_name);
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 #endif
