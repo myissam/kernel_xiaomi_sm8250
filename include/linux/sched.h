028a6be2c183393a00753fa515ab442a402b0732
Julian Liu <wlootlxt123@gmail.com>
2019-08-10 16:49:20 +0800

    sched/tune: Hardcode boost ability as top-app only for DSB
    * This is the only meaningful that boost for top-app process
    * We currently only have such boost
    * update sched_boost for this changes
    
    Signed-off-by: Julian Liu <wlootlxt123@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2293,11 +2293,11 @@ extern inline void restore_inherit_top_app(struct task_struct *p);
 #endif /* CONFIG_MIHW */
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
-int do_stune_boost(char *st_name, int boost, int *slot);
-int do_stune_sched_boost(char *st_name, int *slot);
-int reset_stune_boost(char *st_name, int slot);
+int do_stune_boost(int boost, int *slot);
+int do_stune_sched_boost(int *slot);
+int reset_stune_boost(int slot);
 
-int inline get_sched_boost(char *st_name);
+int inline get_sched_boost(void);
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 #endif
