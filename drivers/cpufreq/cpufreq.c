13515d6022cdba53ac35cedd2424a320e776ea09
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-04 21:14:17 -0800

    cpufreq: Force the LITTLE cluster to only use the default governor
    Since the LITTLE cluster now only has two frequencies, both of which are
    very close to one another, it doesn't make much sense to keep schedutil
    running for it when there's almost no power difference between the two
    frequencies. As such, force the LITTLE cluster to stick to the default
    CPU governor, which is always sanely set to performance.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -792,6 +792,13 @@ static ssize_t store_scaling_governor(struct cpufreq_policy *policy,
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
 		return count;
 
+	/*
+	 * Force the LITTLE CPU cluster to use the default govenor (performance)
+	 * because keeping it at its maximum frequency is best.
+	 */
+	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
+		return count;
+
 	ret = sscanf(buf, "%15s", str_governor);
 	if (ret != 1)
 		return -EINVAL;
