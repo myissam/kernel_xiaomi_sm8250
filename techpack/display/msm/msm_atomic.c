1e779c2d85682f1699a1e5fad85f7333ced29eb8
Sultan Alsawaf <sultan@kerneltoast.com>
2021-02-19 20:32:49 -0800

    drm/msm: Offload commit cleanup onto an unbound worker
    Now that unbound workqueues are affined to the little CPU cluster, the
    cleanup portion of non-blocking commits can be offloaded to little CPUs
    via an unbound worker to reduce latency in the display commit path,
    since it takes up a non-trivial amount of CPU time. This reduces display
    commit latency.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- techpack/display/msm/msm_atomic.c
+++ techpack/display/msm/msm_atomic.c
@@ -32,7 +32,10 @@ struct msm_commit {
 	uint32_t crtc_mask;
 	uint32_t plane_mask;
 	bool nonblock;
-	struct kthread_work commit_work;
+	union {
+		struct kthread_work commit_work;
+		struct work_struct clean_work;
+	};
 };
 
 static inline bool _msm_seamless_for_crtc(struct drm_device *dev,
@@ -115,20 +118,19 @@ static inline bool _msm_seamless_for_conn(struct drm_connector *connector,
 }
 
 /* clear specified crtcs (no longer pending update) */
-static void commit_destroy(struct msm_commit *c)
+static void end_atomic(struct msm_drm_private *priv, uint32_t crtc_mask,
+			uint32_t plane_mask)
 {
-	struct msm_drm_private *priv = c->dev->dev_private;
-	uint32_t crtc_mask = c->crtc_mask;
-	uint32_t plane_mask = c->plane_mask;
-
-	/* End_atomic */
 	spin_lock(&priv->pending_crtcs_event.lock);
 	DBG("end: %08x", crtc_mask);
 	priv->pending_crtcs &= ~crtc_mask;
 	priv->pending_planes &= ~plane_mask;
 	wake_up_all_locked(&priv->pending_crtcs_event);
 	spin_unlock(&priv->pending_crtcs_event.lock);
+}
 
+static void commit_destroy(struct msm_commit *c)
+{
 	if (c->nonblock)
 		kfree(c);
 }
@@ -491,10 +493,14 @@ int msm_atomic_prepare_fb(struct drm_plane *plane,
 	return msm_framebuffer_prepare(new_state->fb, kms->aspace);
 }
 
-extern struct device *connector_kdev;
-void complete_time_generate_event(struct drm_device *dev)
+static void complete_commit_cleanup(struct work_struct *work)
 {
-	sysfs_notify(&connector_kdev->kobj, NULL, "complete_commit_time");
+	struct msm_commit *c = container_of(work, typeof(*c), clean_work);
+	struct drm_atomic_state *state = c->state;
+
+	drm_atomic_state_put(state);
+
+	commit_destroy(c);
 }
 
 /* The (potentially) asynchronous part of the commit.  At this point
@@ -537,13 +543,9 @@ static void complete_commit(struct msm_commit *c)
 
 	kms->funcs->complete_commit(kms, state);
 
-	drm_atomic_state_put(state);
-
 	priv->complete_commit_time = ktime_get()/1000;
 
-	complete_time_generate_event(dev);
-
-	commit_destroy(c);
+	end_atomic(priv, c->crtc_mask, c->plane_mask);
 }
 
 static void _msm_drm_commit_work_cb(struct kthread_work *work)
@@ -561,9 +563,17 @@ static void _msm_drm_commit_work_cb(struct kthread_work *work)
 	 */
 	pm_qos_add_request(&req, PM_QOS_CPU_DMA_LATENCY, 100);
 	SDE_ATRACE_BEGIN("complete_commit");
-	complete_commit(commit);
+	complete_commit(c);
 	SDE_ATRACE_END("complete_commit");
 	pm_qos_remove_request(&req);
+
+	if (c->nonblock) {
+		/* Offload the cleanup onto little CPUs (an unbound wq) */
+		INIT_WORK(&c->clean_work, complete_commit_cleanup);
+		queue_work(system_unbound_wq, &c->clean_work);
+	} else {
+		complete_commit_cleanup(&c->clean_work);
+	}
 }
 
 static struct msm_commit *commit_init(struct drm_atomic_state *state,
@@ -639,6 +649,7 @@ static void msm_atomic_commit_dispatch(struct drm_device *dev,
 		 * ensure that SW and HW state don't get out of sync.
 		 */
 		complete_commit(commit);
+		complete_commit_cleanup(&commit->clean_work);
 	} else if (!nonblock) {
 		kthread_flush_work(&commit->commit_work);
 	}
