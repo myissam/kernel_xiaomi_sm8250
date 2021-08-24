596708edc9504c84dfd1695b1d831c8e4944764f
Emanuel <emanuelghub@gmail.com>
2021-08-25 01:15:02 +0400

    Treewide: Fix direct references to HZ

--- drivers/video/fbdev/msm/mdss_dsi.c
+++ drivers/video/fbdev/msm/mdss_dsi.c
@@ -3062,9 +3062,8 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 
 		if (IS_ENABLED(CONFIG_MSM_DBA) &&
 			pdata->panel_info.is_dba_panel) {
-			queue_delayed_work(ctrl_pdata->workq,
-				&ctrl_pdata->dba_work, HZ);
-                                    &ctrl_pdata->dba_work, msecs_to_jiffies(1000));
+				queue_delayed_work(ctrl_pdata->workq,
+					&ctrl_pdata->dba_work, msecs_to_jiffies(1000));
 		}
 		break;
 	case MDSS_EVENT_DSI_TIMING_DB_CTRL:
