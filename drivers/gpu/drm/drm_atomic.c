01c48f71b775e21b4b5d7e0590618c4ef73913be
myissam <myissam07@gmail.com>
2021-09-12 11:33:55 +0100

    Revert "cpu/drm:boost DDR|cpu to adapt with CAF's variations"
    This reverts commit d48cb1a1cbbf272e5a5e6b7397d19ef5ae91a36f.

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2599,7 +2599,6 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
-	/* Boost DDR when committing a new frame */
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY))
 		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
 
