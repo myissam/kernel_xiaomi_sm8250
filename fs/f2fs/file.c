cb19d422674d8abdd709f23905f69f0fd2a56864
myissam <myissam07@gmail.com>
2021-11-07 21:39:06 +0100

    fs: {ext4, f2fs}: Remove CAF tracings
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    (cherry picked from commit 7b9cf5ece723a1445ebbd472a1e8063e48a83a02)
    Change-Id: I6539321164d96fff98a0180b636456e9cb6e07e4
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -37,7 +37,6 @@
 #include "gc.h"
 #include "iostat.h"
 #include <trace/events/f2fs.h>
-#include <trace/events/android_fs.h>
 #include <uapi/linux/f2fs.h>
 
 static vm_fault_t f2fs_filemap_fault(struct vm_fault *vmf)
@@ -284,15 +283,6 @@ static int f2fs_do_sync_file(struct file *file, loff_t start, loff_t end,
 
 	trace_f2fs_sync_file_enter(inode);
 
-	if (trace_android_fs_fsync_start_enabled()) {
-		char *path, pathbuf[MAX_TRACE_PATHBUF_LEN];
-
-		path = android_fstrace_get_pathname(pathbuf,
-				MAX_TRACE_PATHBUF_LEN, inode);
-		trace_android_fs_fsync_start(inode,
-				current->pid, path, current->comm);
-	}
-
 	start_time = ktime_get();
 
 	if (S_ISDIR(inode->i_mode))
@@ -418,28 +408,12 @@ out:
 	delta = ktime_sub(ktime_get(), start_time);
 	duration = (unsigned long long) ktime_to_ns(delta) / (1000 * 1000);
 
-	/* print slow fsync spend more than 1s */
-	if (duration > 1000) {
-		char *file_path, file_pathbuf[MAX_TRACE_PATHBUF_LEN];
-
-		file_path = android_fstrace_get_pathname(file_pathbuf,
-				MAX_TRACE_PATHBUF_LEN, inode);
-		pr_info("[f2fs] slow fsync: %llu ms, cp_reason: %s, "
-			"datasync = %d, ret = %d, comm: %s: (uid %u, gid %u), "
-			"entry: %s", duration, f2fs_cp_reasons[cp_reason],
-			datasync, ret, current->comm,
-			from_kuid_munged(&init_user_ns, current_fsuid()),
-			from_kgid_munged(&init_user_ns, current_fsgid()),
-			file_path);
-	}
-
 #if defined(CONFIG_UFSTW)
 	if (turbo_set)
 		bdev_clear_turbo_write(sbi->sb->s_bdev);
 #endif
 	trace_f2fs_sync_file_exit(inode, cp_reason, datasync, ret);
 	stat_inc_sync_file_count(sbi);
-	trace_android_fs_fsync_end(inode, start, end - start);
 
 	return ret;
 }
