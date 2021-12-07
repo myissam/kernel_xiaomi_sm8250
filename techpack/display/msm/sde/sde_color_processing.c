5222190e64010ebbbc564d28dce311f5d74207bd
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-12-07 11:04:46 +0530

    Merge tag 'c1796a13ec6eabc9ea1f2960114c5351309280c9' into android11-stable
    "LA.UM.9.12.r1-13500.01-SMxx50.QSSI12.0"
    
    * tag 'c1796a13ec6eabc9ea1f2960114c5351309280c9':
      disp: msm: sde: protect file private structure with mutex lock
      disp: msm: sde: protect file private structure with mutex lock
      disp: msm: dp: parse displayport label dt property
      disp: msm: sde: handle partial update transitions in rounded corner
    
    Change-Id: Iecdb46715246d0dd0605e1db209415cd857f6377

--- techpack/display/msm/sde/sde_color_processing.c
+++ techpack/display/msm/sde/sde_color_processing.c
@@ -1677,20 +1677,24 @@ static int sde_cp_crtc_set_pu_features(struct drm_crtc *crtc, bool *need_flush)
 	}
 
 	/* early return if not a partial update frame or no change in rois */
-	if (sde_crtc_state->user_roi_list.num_rects == 0) {
+	if ((sde_crtc_state->user_roi_list.num_rects == 0) &&
+		(sde_crtc->cached_user_roi_list.num_rects == 0)) {
 		DRM_DEBUG_DRIVER("no partial update required\n");
-		memset(&sde_crtc_state->cached_user_roi_list, 0,
+		return 0;
+	} else if (sde_crtc_state->user_roi_list.num_rects == 0) {
+		DRM_DEBUG_DRIVER("transition from PU to full update\n");
+		memset(&sde_crtc->cached_user_roi_list, 0,
 				sizeof(struct msm_roi_list));
-		return 0;
-	}
-
-	sde_kms_rect_merge_rectangles(&sde_crtc_state->user_roi_list,
-			&user_rect);
-	sde_kms_rect_merge_rectangles(&sde_crtc_state->cached_user_roi_list,
-			&cached_rect);
-	if (sde_kms_rect_is_equal(&user_rect, &cached_rect)) {
-		DRM_DEBUG_DRIVER("no change in list of ROIs\n");
-		return 0;
+	} else {
+		sde_kms_rect_merge_rectangles(&sde_crtc_state->user_roi_list,
+				&user_rect);
+		sde_kms_rect_merge_rectangles(
+				&sde_crtc->cached_user_roi_list,
+				&cached_rect);
+		if (sde_kms_rect_is_equal(&user_rect, &cached_rect)) {
+			DRM_DEBUG_DRIVER("no change in list of ROIs\n");
+			return 0;
+		}
 	}
 
 	catalog = get_kms(&sde_crtc->base)->catalog;
@@ -1732,9 +1736,11 @@ static int sde_cp_crtc_set_pu_features(struct drm_crtc *crtc, bool *need_flush)
 		}
 	}
 
-	memcpy(&sde_crtc_state->cached_user_roi_list,
-			&sde_crtc_state->user_roi_list,
-			sizeof(struct msm_roi_list));
+	if (sde_crtc_state->user_roi_list.num_rects != 0) {
+		memcpy(&sde_crtc->cached_user_roi_list,
+				&sde_crtc_state->user_roi_list,
+				sizeof(struct msm_roi_list));
+	}
 
 	return 0;
 }
