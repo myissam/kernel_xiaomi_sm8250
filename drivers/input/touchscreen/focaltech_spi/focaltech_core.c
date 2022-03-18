b01ed9f43a070481240fcb55540158a5cd4cadf4
myissam <myissam07@gmail.com>
2022-03-18 16:16:03 +0100

    input: touchscreen: focaltech: Optimize interrupt CPU usage
    Instead of preventing all CPUs from entering deep idle states, only
    force the ones needed for touch processing to stay awake. To process a
    touch interrupt, SPI reads are needed which make use of a completion
    interrupt from the SPI controller (since SPI I/O is asynchronous). This
    interrupt and the touch controller's interrupt are all that need to be
    sped up. Only waking up CPUs that process these interrupts significantly
    cuts down the overall power consumption of touch interrupt processing.
    
    Additionally, speed up the SPI interrupt by affining it to the big CPU
    cluster. SPI interrupts are too numerous to place them on the prime CPU
    cluster, so they get the big CPU cluster instead.
    
    Commit description by Sultan Alsawaf <sultan@kerneltoast.com>
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- drivers/input/touchscreen/focaltech_spi/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_spi/focaltech_core.c
@@ -725,7 +725,15 @@ static irqreturn_t fts_irq_handler(int irq, void *data)
 	struct fts_ts_data *ts_data = fts_data;
 
 	pm_stay_awake(fts_data->dev);
+
+	pm_qos_update_request(&ts_data->pm_touch_req, 100);
+	pm_qos_update_request(&ts_data->pm_spi_req, 100);
+
 	fts_irq_read_report();
+
+	pm_qos_update_request(&ts_data->pm_touch_req, PM_QOS_DEFAULT_VALUE);
+	pm_qos_update_request(&ts_data->pm_spi_req, PM_QOS_DEFAULT_VALUE);
+
 	pm_relax(fts_data->dev);
 	return IRQ_HANDLED;
 }
@@ -2338,6 +2346,17 @@ static int fts_ts_probe(struct spi_device *spi)
 		return -ENOMEM;
 	}
 
+	ts_data->pm_spi_req.type = PM_QOS_REQ_AFFINE_IRQ;
+	ts_data->pm_spi_req.irq = geni_spi_get_master_irq(spi);
+	irq_set_perf_affinity(ts_data->pm_spi_req.irq, IRQF_PERF_AFFINE);
+	pm_qos_add_request(&ts_data->pm_spi_req, PM_QOS_CPU_DMA_LATENCY,
+			PM_QOS_DEFAULT_VALUE);
+
+	ts_data->pm_touch_req.type = PM_QOS_REQ_AFFINE_IRQ;
+	ts_data->pm_touch_req.irq = spi->irq;
+	pm_qos_add_request(&ts_data->pm_touch_req, PM_QOS_CPU_DMA_LATENCY,
+			PM_QOS_DEFAULT_VALUE);
+
 	fts_data = ts_data;
 	ts_data->spi = spi;
 	ts_data->dev = &spi->dev;
@@ -2350,6 +2369,8 @@ static int fts_ts_probe(struct spi_device *spi)
 	ret = fts_ts_probe_entry(ts_data);
 	if (ret) {
 		FTS_ERROR("Touch Screen(SPI BUS) driver probe fail");
+		pm_qos_remove_request(&ts_data->pm_touch_req);
+		pm_qos_remove_request(&ts_data->pm_spi_req);
 		kfree_safe(ts_data);
 		return ret;
 	}
