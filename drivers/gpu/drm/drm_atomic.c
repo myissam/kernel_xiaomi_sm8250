a8a31b4e27b99a7faae5668fe388c8e55e7b287a
myissam <myissam07@gmail.com>
2022-03-22 14:32:40 +0100

    drm_atomic : devfreq only

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2575,9 +2575,9 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 	struct drm_out_fence_state *fence_state;
 	int ret = 0;
 	unsigned int i, j, num_fences;
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+/* #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	int slot;
-#endif
+#endif */
 
 	/* disallow for drivers not supporting atomic: */
 	if (!drm_core_check_feature(dev, DRIVER_ATOMIC))
@@ -2608,11 +2608,11 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 	/* Boost CPU and DDR when committing a new frame */
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
 			devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+/* #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 			if ((sysctl_sched_boost) || df_boost_within_input(3250)) {
 				do_stune_sched_boost(&slot);
 #endif
-		}
+		} */
 	}
 
 	drm_modeset_acquire_init(&ctx, DRM_MODESET_ACQUIRE_INTERRUPTIBLE);
