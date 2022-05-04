9a04a8bb3d282f6215a8440cb9a97474047423ab
myissam <myissam07@gmail.com>
2022-05-04 18:50:52 +0000

    kernel: Boost DDR/CPU

--- kernel/fork.c
+++ kernel/fork.c
@@ -96,6 +96,8 @@
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
 #include <linux/simple_lmk.h>
+#include <linux/devfreq_boost.h>
+#include <linux/cpu_input_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2373,6 +2375,12 @@ long _do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
+
+	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
+	if (task_is_zygote(current)) {
+		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
+		cpu_input_boost_kick();
+	}	
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
