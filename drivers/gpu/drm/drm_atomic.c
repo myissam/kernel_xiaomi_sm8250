8d19971bbc3f99e7f3b60253d86b86e9ac47d871
myissam <myissam07@gmail.com>
2022-05-28 13:48:55 +0100

    drm/drm_atomic: Fallback to boosting CPU and DDR bus within 3s of last interaction
    Introduce a secondary check to ensure that, if sched_boost fails for
    any reason, we can still kick off some form of interaction boost
    following user input.
    
    This is a less refined and more inaccurate approach which may not
    cover extended scrolls and flings, but it's better than nothing.
    
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>
    Co-authored-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -34,7 +34,6 @@
 #include <linux/sync_file.h>
 #include <linux/devfreq_boost.h>
 #include <linux/cpu_input_boost.h>
-#include <linux/sched/sysctl.h>
 
 #include "drm_crtc_internal.h"
 #include "drm_internal.h"
@@ -2601,10 +2600,10 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 		return -EINVAL;
 
 	/* Boost CPU and DDR when committing a new frame if sched_boost > 0 */
-	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) &&
-			sysctl_sched_boost) {
-		cpu_input_boost_kick();
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
 		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
+		if (time_before(jiffies, last_input_time + msecs_to_jiffies(3000)))
+		    cpu_input_boost_kick();
 	}
 
 	drm_modeset_acquire_init(&ctx, DRM_MODESET_ACQUIRE_INTERRUPTIBLE);
